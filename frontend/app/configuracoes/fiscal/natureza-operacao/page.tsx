692a23c1f771bf4bf2e8636b0ef6f3c06fee46b8
bvfelipebv <bvfelipebv@outlook.com>
2025-10-28 18:21:48 +0000

    feat: add configuration pages with sidebar and breadcrumb navigation
    - Implemented the main configuration page with sidebar and section cards.
    - Created system configuration page with interactive chart area.
    - Added user management page with sidebar and breadcrumb navigation.
    - Developed fiscal pages for NFC-e and NF-e with consistent layout.
    - Introduced supplier management pages with sidebar and section cards.
    - Created product management pages including categories, brands, and new product forms.
    - Implemented sales management pages with order tracking and new sale forms.
    - Added reporting pages for clients, products, and sales with interactive charts.

---
+++ frontend/app/configuracoes/fiscal/natureza-operacao/page.tsx
@@ -0,0 +1,59 @@
+import { AppSidebar } from "@/components/app-sidebar"
+import { ChartAreaInteractive } from "@/components/chart-area-interactive"
+import { SectionCards } from "@/components/section-cards"
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb"
+import { Separator } from "@/components/ui/separator"
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar"
+
+
+export default function Page() {
+  return (
+    <SidebarProvider
+      style={
+        {
+          "--sidebar-width": "350px",
+        } as React.CSSProperties
+      }
+    >
+      <AppSidebar />
+      <SidebarInset>
+        <header className="bg-background sticky top-0 flex shrink-0 items-center gap-2 border-b p-4">
+          <SidebarTrigger className="-ml-1" />
+          <Separator
+            orientation="vertical"
+            className="mr-2 data-[orientation=vertical]:h-4"
+          />
+          <Breadcrumb>
+            <BreadcrumbList>
+              <BreadcrumbItem>
+                <BreadcrumbPage>Natureza de Operação</BreadcrumbPage>
+              </BreadcrumbItem>
+            </BreadcrumbList>
+          </Breadcrumb>
+        </header>
+        <div className="flex flex-1 flex-col">
+          <div className="@container/main flex flex-1 flex-col gap-2">
+            <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+              <SectionCards />
+              <div className="px-4 lg:px-6">
+                <ChartAreaInteractive />
+              </div>
+              
+            </div>
+          </div>
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
+  )
+}
